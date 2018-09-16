370f8cbbab215144e3875f4328efc050667f6e2c
Vantan LLC <vantan.llc@gmail.com>
2018-09-16 16:51:19 +0530

    Initial Commit

---
+++ Home Research/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Home Research
+//
+//  Created by Thinh Luong on 9/16/18.
+//  Copyright © 2018 Vantan LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
