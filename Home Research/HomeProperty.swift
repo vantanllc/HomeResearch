61757401ff4c7aff94470b48c95d527cf9ef651b
Vantan LLC <vantan.llc@gmail.com>
2018-09-16 17:24:53 +0530

    Add first test

---
+++ Home Research/HomeProperty.swift
@@ -0,0 +1,13 @@
+//
+//  HomeProperty.swift
+//  Home Research
+//
+//  Created by Thinh Luong on 9/16/18.
+//  Copyright © 2018 Vantan LLC. All rights reserved.
+//
+
+import Foundation
+
+class HomeProperty {
+  
+}
---
+++ Home ResearchTests/HomePropertyTests.swift
@@ -0,0 +1,16 @@
+//
+//  HomePropertyTests.swift
+//  Home ResearchTests
+//
+//  Created by Thinh Luong on 9/16/18.
+//  Copyright © 2018 Vantan LLC. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Home_Research
+
+class HomePropertySpec: QuickSpec {
+  override func spec() {
+  }
+}
