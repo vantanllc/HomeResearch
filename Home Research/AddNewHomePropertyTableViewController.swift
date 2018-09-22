dcbe784bc13743bcacda8ee2d46d936c453ff0c3
Vantan LLC <vantan.llc@gmail.com>
2018-09-22 19:28:15 +0530

    add new home property modal view

---
+++ Home Research/AddNewHomePropertyTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddNewHomePropertyTableViewController.swift
+//  Home Research
+//
+//  Created by Thinh Luong on 9/22/18.
+//  Copyright © 2018 Vantan LLC. All rights reserved.
+//
+
+import UIKit
+
+class AddNewHomePropertyTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+  @IBAction func dismissViewController(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+  }
+}
